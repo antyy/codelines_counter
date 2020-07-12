71d1a37d49928709870180f6b433644b3052742d
antyy <john.kudrenko@gmail.com>
2020-07-12 18:27:08 +0300

    minor fix

--- src/test/java/com/techtask/counter/DirectoryRowCounterServiceTest.java
+++ src/test/java/com/techtask/counter/DirectoryRowCounterServiceTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static com.google.common.truth.Truth.assertThat;
+import static java.util.Arrays.asList;
 
 class DirectoryRowCounterServiceTest {
     private DirectoryRowCounterService directoryRowCounterService;
@@ -18,14 +21,14 @@ class DirectoryRowCounterServiceTest {
     @DisplayName("Should return five rows for complex tree directory structure")
     public void countRows_directoryTReeStructure_5directories() {
         String root = "src/test/resources/root";
-        String expected = "root : 2 SomeRoot.java : 2 \n" +
-                "level2_11 : 16 MultilineComment.java : 6 CornerCases.java : 10 \n" +
-                "level1_2 : 62 Service.java : 62 \n" +
-                "level2_111 : 5 SingleLineComment.java : 5 \n" +
-                "level1_1 : 18 TestClass.java : 8 SomeClass.java : 10 \n";
+        String[] expected = {"root : 2 SomeRoot.java : 2 ",
+                "level2_11 : 16 MultilineComment.java : 6 CornerCases.java : 10 ",
+                "level1_2 : 62 Service.java : 62 ",
+                "level2_111 : 5 SingleLineComment.java : 5 ",
+                "level1_1 : 18 TestClass.java : 8 SomeClass.java : 10 "};
         String actual = directoryRowCounterService.countRows(root);
-
-        assertThat(actual).isEqualTo(expected);
+        List<String> actualList = asList(actual.split("\n"));
+        assertThat(actualList).containsExactly(expected);
     }
 
     @Test
